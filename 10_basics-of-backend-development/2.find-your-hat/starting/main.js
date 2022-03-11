8916f15df645015070aeba5faff8b5714df66126
Taylor Conkle <t.conkle01@gmail.com>
2022-03-11 07:39:14 -0600

    Added print() to Field class.

--- 10_basics-of-backend-development/2.find-your-hat/starting/main.js
+++ 10_basics-of-backend-development/2.find-your-hat/starting/main.js
@@ -1,4 +1,4 @@
-const prompt = require('prompt-sync')({sigint: true});
+const prompt = require('prompt-sync')({ sigint: true });
 
 const hat = '^';
 const hole = 'O';
@@ -6,5 +6,11 @@ const fieldCharacter = '░';
 const pathCharacter = '*';
 
 class Field {
-  
+    constructor(field) {
+        this._field = field;
+    }
+    print() {
+        // ? Convert 2D Array to String
+        console.log(this._field);
+    }
 }
